4c36eab34f8d48a4f7d07287697b105fedbf49a1
Aman Kumar <ak3230531@gmail.com>
2024-11-13 18:56:54 +0530

    'I learn .concat() in array'

--- 02_basics/array2.js
+++ 02_basics/array2.js
@@ -20,19 +20,25 @@ const one_array = another_array.flat(Infinity)
 
 
 
-console.log(Array.isArray([]));
-console.log(Array.from("Aman"));
-console.log(Array.from({name: "Aman Kumar"}));
+// console.log(Array.isArray([]));
+// console.log(Array.from("Aman"));
+// console.log(Array.from({name: "Aman Kumar"}));
 
 
-console.log(Array.isArray("Hitesh"));
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+// console.log(Array.isArray("Hitesh"));
+// console.log(Array.from("Hitesh"))
+// console.log(Array.from({name: "hitesh"})) // interesting
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1,score2,score3));
+// console.log(Array.of(score1,score2,score3));
 
 // console.log(Array.of(score1, score2, score3));
+
+let a1 = [1,2,3]
+let a2 = [4,5,6]
+let a3 = [7,8,9]
+ // .concat() returns a new it doesn't change the orignal array
+console.log(a1.concat(a2,a3));
