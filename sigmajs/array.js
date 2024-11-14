68aa9d58fce808d416eb32e48cacd679c66e5b1c
Aman Kumar <ak3230531@gmail.com>
2024-11-14 21:41:55 +0530

    'revising array concepts'

---
+++ sigmajs/array.js
@@ -0,0 +1,51 @@
+const array = [11,22,13,14,52,62,27,68,39, 199]
+
+// console.log(typeof array.toString());
+// console.log(typeof array);
+
+// console.log(array.join('_'));
+
+// delete array[9]
+// console.log(array);
+
+let a1 = [1,2,3]
+let a2 = [4,5,6]
+let a3 = [7,8,9]
+
+// console.log(a1.concat(a2,a3,array));
+
+
+// console.log(array);
+
+array.sort((a, b) => a - b ) // array.sort() ->It sort as string 
+// console.log(array);
+
+const numbers = [1,2,3,4,5]
+// numbers.splice(2,1,22,23)
+// console.log(numbers);
+
+
+console.log(numbers.slice(2)); // slice didn't modify original string 
+console.log(numbers.reverse());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
