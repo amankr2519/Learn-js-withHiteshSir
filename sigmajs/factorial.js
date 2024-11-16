adf4c91e6b3f1c113685d04e890e182861599150
Aman Kumar <ak3230531@gmail.com>
2024-11-16 16:29:50 +0530

    by reduce method

--- sigmajs/factorial.js
+++ sigmajs/factorial.js
@@ -1,11 +1,22 @@
 let n = 5 
 // factorial by loop
-let factorial = 1
-for(let i = 1 ; i<=n ; i++){
-  factorial = factorial * i
+// let factorial = 1
+// for(let i = 1 ; i<=n ; i++){
+//   factorial = factorial * i
+// }
+// console.log(factorial);
+
+
+
+// by reduce 
+
+function factorial(num){
+  let arr = Array.from(Array(num+1).keys())
+  let c = arr.slice(1,).reduce((a,b)=> a*b)
+  return c 
 }
-console.log(factorial);
 
-
+let result = factorial(n)
+console.log(result);
 
 
