6aa0265d8a99c914e59640b5b8530c3ce548bbc5
Aman Kumar <ak3230531@gmail.com>
2024-11-16 15:52:53 +0530

    fr

---
+++ sigmajs/mfr.js
@@ -0,0 +1,14 @@
+const arr = [1,2,3,4,5]
+
+// map 
+const arr1 = arr.map(e => e*e)
+console.log(arr);
+console.log(arr1);
+
+// filter
+
+const arr2 = arr.filter(e => e>2)
+console.log(arr2);
+
+
+
