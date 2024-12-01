d80727de7a2b8e69f5879bc876de1ef97b101c85
Aman Kumar <ak3230531@gmail.com>
2024-12-01 22:34:53 +0530

    practice

---
+++ practice/p1.js
@@ -0,0 +1,36 @@
+let object = {
+  name : 'Aman',
+  enrollment_no : 123,
+  branch : 'Aiml b2'
+}
+
+object = [1,2,3,4]
+
+firstName = 'Aman'
+
+for (const key in object) {
+  // if (Object.prototype.hasOwnProperty.call(object, key)) {
+    
+  // }
+  const element = object[key];
+  console.log(element)
+}
+
+
+// for of use for iterable items like array and string 
+
+for (const element of firstName) {
+  console.log(element);
+  
+  
+}
+let i = 1
+while (i<10) {
+  console.log(i);
+  i++
+  
+}
+
+// do{
+//   // condition
+// }while()
