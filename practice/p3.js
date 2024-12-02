5b532948dfe536c5104c89a1212450803ee2fcd8
Aman Kumar <ak3230531@gmail.com>
2024-12-02 22:01:38 +0530

    practice splicing and slice in js

---
+++ practice/p3.js
@@ -0,0 +1,24 @@
+// array 
+
+const arr = [9,8,6,4,7,3,2,1,5]
+
+arr.sort()  // alphabatically
+
+console.log(arr);
+
+
+// splice
+arr.splice(0,2,11,21)
+console.log(arr);
+
+// slice
+
+const num = [1,2,3,4]
+// num.slice(2)
+
+
+
+num.reverse()
+console.log(num);
+
+
